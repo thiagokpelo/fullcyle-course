cb4e33a3238f494dd0678d765e4943bb61cd3651
Thiago Capelo <thiagokpelo@gmail.com>
2022-07-05 19:24:23 -0300

    test: add new case to go and node code

--- ci/go/sum.go
+++ ci/go/sum.go
@@ -3,13 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(sum(1, 2))
+  fmt.Println(sum(1, 2))
 }
 
 func sum(a, b int) int {
-	return a + b
+  return a + b
 }
 
 func sub(a, b int) int {
-	return a - b
+  return a - b
+}
+
+func mult(a, b int) int {
+  return a * b
+}
+
+func div(a, b int) int {
+  return a / b
 }
---
+++ ci/node/src/div.js
@@ -0,0 +1,5 @@
+function div(a, b) {
+  return a / b
+}
+
+module.exports = div
